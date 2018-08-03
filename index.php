b8b0f54ad362988811c40be47756e26cee34ee16
Jekaterina <misstery@inbox.lv>
2018-08-04 00:52:50 +0300

    DB change

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 //error_reporting(E_ALL);
-$pdo = new PDO("mysql:host=localhost;dbname=global;charset=utf8", "JLoseva", "neto1801");
+$pdo = new PDO("mysql:host=localhost;dbname=JLoseva;charset=utf8", "JLoseva", "neto1801");
 if (!$pdo)
     {
         die('Невозможно подключиться');
